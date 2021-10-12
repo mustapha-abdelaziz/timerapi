4c565033d7902a4f1cd71fd7b8e725213ebfdec2
mustapha-abdelaziz <contact.mustafa.aziz@gmail.com>
2021-10-12 15:46:32 +0100

    adding cors middlware

--- app.js
+++ app.js
@@ -2,33 +2,23 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const socket = require("socket.io");
+const cors = require('cors')
+
+
+app.use(cors())
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-
-let time = "dsadsdasfqw";
-
-app.use("/", (req, res)=>{
+app.use("/", (req, res) => {
 	res.send("<h1>listening to port: </h1>");
 });
 
-app.post("/api/setTimer", (req, res) => {
-	time = req.body.mediaDuration;
-});
-
-app.get("/api/gettimer", (req, res) => {
-	res.status(200).send("hello world " + time);
-});
-
-const server = app.listen(port, () => {
-	console.log("listening to port ", port);
-});
+const server = app.listen(port);
 
 const io = socket(server);
-
 io.on("connection", function (socket) {
 	let rawdata = fs.readFileSync("time.json");
 	let currentTimerTimestamp = JSON.parse(rawdata);
@@ -37,11 +27,10 @@ io.on("connection", function (socket) {
 	socket.on("counter", (time) => {
 		io.sockets.emit("emited-counter", time);
 
-		data ={
-			"timer": time,
-			"submittedOn": Date.now()
-		}
-
+		data = {
+			timer: time,
+			submittedOn: Date.now(),
+		};
 		fs.writeFileSync("time.json", JSON.stringify(data));
 	});
 });
