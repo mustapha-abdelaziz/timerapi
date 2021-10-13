a8b8b04f11bdfc344650d8c8287cbc51e7cf9db7
mustapha-abdelaziz <contact.mustafa.aziz@gmail.com>
2021-10-13 16:20:53 +0100

    add get timer endpoint

--- app.js
+++ app.js
@@ -1,39 +1,34 @@
 const express = require("express");
 const fs = require("fs");
 const socket = require("socket.io");
-const cors = require('cors')
+const cors = require("cors");
 const app = express();
 
-app.use(cors({origin: 'https://obs-timer-api.herokuapp.com'}))
+app.use(cors({ origin: "https://obs-timer-api.herokuapp.com" }));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
-
-app.use("/", (req, res) => {
-	res.send("<h1>listening to port: </h1>");
+app.get("/api/timer", (req, res) => {
+	let rawData = fs.readFileSync("time.json");
+	let obj = JSON.parse(rawData);
+	res.json(obj);
 });
+
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port);
 
 const io = socket(server);
 io.on("connection", function (socket) {
-	
-	requestSentAt = socket.handshake.query.connectedAt
-	let rawdata = fs.readFileSync("time.json");
-	let currentTimerTimestamp = JSON.parse(rawdata);
+	requestSentAt = socket.handshake.query.connectedAt;
 
-
-	io.sockets.emit("server-connection", currentTimerTimestamp, requestSentAt , Date.now());
-
-	socket.on("counter", (time) => {
-		io.sockets.emit("emited-counter", time);
-
+	socket.on("counter", (time, string) => {
 		data = {
 			timer: time,
 			submittedOn: Date.now(),
 		};
+		io.sockets.emit("emited-counter", data);
 
 		fs.writeFileSync("time.json", JSON.stringify(data));
 	});
